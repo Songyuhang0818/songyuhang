04b29c8e5586de02cc2791127c0d5213a05f3d23
Songyuhang0818 <278022673@qq.com>
2018-01-26 11:03:36 +0800

    20180126

---
+++ webpack/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const devServer = require('webpack-dev-server');
+module.exports = {
+    entry:{
+        index:'./src/index.js'
+    },
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].js'
+    },
+    module:{
+
+    },
+    plugins:{
+
+    },
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'localhost',
+        port:'8082'
+    }
+}
