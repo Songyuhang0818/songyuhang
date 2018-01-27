e3487b1532a82163c45f3bca1d5e836a399eaf42
Songyuhang0818 <278022673@qq.com>
2018-01-27 11:59:49 +0800

    20180127

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,10 @@
+import css from './css/index.css';
+import sass from './css/common.scss';
 document.write('dfsdfsdgsfgdg');
+Person = (name,age) => {
+    this.name = name;
+    this.age = age;
+}
+var p1 = new Person('zs',10);
+
+console.log(p1.name);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,17 +1,80 @@
 const path = require('path');
+const webpack = require('webpack');
 const devServer = require('webpack-dev-server');
 const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob');
+const PurifyCSSPlugin = require("purifycss-webpack");
 module.exports = {
     entry:{
-        index:'./src/index.js'
+        index:'./src/index.js',
+        index2:'./src/index2.js'
     },
     output:{
         path: path.resolve(__dirname,'dist'),
-        filename:'[name].js'
+        filename:'[name].js',
+        publicPath:'http://localhost:8081/'
     },
     module:{
-
-    },
+        rules: [
+            {
+                test:/\.css$/,
+                // use: ['style-loader','css-loader']
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:[{
+                        loader:"css-loader",
+                        options:{importLoaders:1}
+                    },'postcss-loader']
+                })
+            },{
+                test:/\.(png|jpg|gif)/,
+                use:[{
+                    loader:'url-loader',
+                    options:{
+                        limit:500,
+                        outputPath:'images/'
+                    }
+                }]
+            },{
+                test:/\.(htm|html)$/i,
+                loader:'html-withimg-loader'
+            },{
+                test:/\.scss/,
+                // use:[
+                //     {
+                //         loader:'style-loader'
+                //     },
+                //     {
+                //         loader:'css-loader'
+                //     },
+                //     {
+                //         loader:'sass-loader'
+                //     }
+                // ]
+                use:ExtractTextPlugin.extract({
+                    use:[{
+                        loader:"css-loader"
+                    },{
+                        loader:"sass-loader"
+                    }],
+                    fallback:"style-loader"
+                })
+            },{
+                test:/\.(jsk|js)$/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:[
+                            "es2015","react"
+                        ]
+                    }
+                },
+                exclude:/node_modules/
+            }
+        ]
+    }, 
     plugins:[
         new HtmlPlugin({
             minify:{
@@ -21,6 +84,12 @@ module.exports = {
             template:'./src/index.html',
             chunks:['index']
         }),
+        new ExtractTextPlugin("css/index.css"),
+        new UglifyJsPlugin(),
+        new PurifyCSSPlugin({
+            paths:glob.sync(path.join(__dirname,'src/*.html'))
+        }),
+        new webpack.BannerPlugin('航哥注释')
     ],
     devServer:{
         contentBase:path.resolve(__dirname,'dist'),
